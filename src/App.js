1187f7035d271096e93c340c50eb000c14b391bb
Jedediah Arnold <jarnold910@gmail.com>
2020-12-19 10:53:24 -0500

    fix redirect issue when logged in

--- src/App.js
+++ src/App.js
@@ -14,25 +14,40 @@ export const LOCAL_TOKEN = "jobly-token";
 function App() {
   const [token, setToken] = useLocalStorage(LOCAL_TOKEN);
   const [currentUser, setCurrentUser] = useState();
+  const [infoLoaded, setInfoLoaded] = useState(false);
 
-  useEffect(() => {
+  useEffect(function loadUserInfo() {
+    console.debug("App useEffect loadUserInfo", "token=", token);
+
     async function getCurrentUser() {
-      try {
-        JoblyApi.token = token;
-        let { username } = decode(token);
-        let currentUser = await JoblyApi.getCurrentUser(username);
-        setCurrentUser(currentUser);
-      } catch (e) {
-        setCurrentUser(null)
+      if (token) {
+        try {
+          let { username } = decode(token);
+          // put the token on the Api class so it can use it to call the API.
+          JoblyApi.token = token;
+          let currentUser = await JoblyApi.getCurrentUser(username);
+          setCurrentUser(currentUser);
+        } catch (err) {
+          console.error("App loadUserInfo: problem loading", err);
+          setCurrentUser(null);
+        }
       }
+      setInfoLoaded(true);
     }
+
+    // set infoLoaded to false while async getCurrentUser runs; once the
+    // data is fetched (or even if an error happens!), this will be set back
+    // to false to control the spinner.
+    setInfoLoaded(false);
     getCurrentUser();
-  }, [token])
+  }, [token]);
 
   const handleLogOut = () => {
     setCurrentUser(null);
     setToken(null);
   };
+
+  if (!infoLoaded) return  <div>Loading....</div>;
 
   return (
     <div className="App">
